246bd6bc16258941f1bce09d766343f8f89c1b3a
kortkamp <marcelusmedius@gmail.com>
2022-05-14 14:27:28 -0300

    feat: add users repositories

---
+++ src/shared/container/index.ts
@@ -0,0 +1,10 @@
+import '@shared/container/providers';
+
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { container } from 'tsyringe';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
