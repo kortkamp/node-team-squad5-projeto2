a1d84cc5ec4102578650e1f0cb8719ee8268ff4b
kortkamp <marcelusmedius@gmail.com>
2022-05-15 16:29:05 -0300

    feat: add products routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,3 +1,4 @@
+import { productsRoutes } from '@modules/products/infra/http/routes/products.routes';
 import { sessionsRoutes } from '@modules/users/infra/http/routes/sessions.routes';
 import { usersRoutes } from '@modules/users/infra/http/routes/users.routes';
 import { Router } from 'express';
@@ -6,5 +7,6 @@ const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
+routes.use('/products', productsRoutes);
 
 export { routes };
