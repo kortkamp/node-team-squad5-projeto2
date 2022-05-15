c19a6238061871eb5e5863224895434807d78e22
kortkamp <marcelusmedius@gmail.com>
2022-05-15 13:58:30 -0300

    feat: add admin seeding

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,25 @@
+import { ICreateUserDTO } from '@modules/users/dtos/ICreateUserDTO';
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
+
+import 'dotenv/config';
+import { AppDataSource } from '..';
+
+async function create() {
+  await AppDataSource.initialize();
+
+  const usersRepository = new UsersRepository();
+
+  const admin: ICreateUserDTO = {
+    email: 'admin@template.com',
+    name: 'Admin',
+    company: 'Default Company',
+    CNPJ: '64.182.592/0001-92',
+    password: await hash('123456', 8),
+  };
+
+  await usersRepository.create(admin);
+  await AppDataSource.destroy();
+}
+
+create().then(() => console.log('User admin Created!'));
