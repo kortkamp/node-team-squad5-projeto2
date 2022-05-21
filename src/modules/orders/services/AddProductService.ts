5d426e269bc800da63d618ece19f9dc0b7ec6bf2
kortkamp <marcelusmedius@gmail.com>
2022-05-21 11:29:17 -0300

    feat: remove product from stock when adding to order

--- src/modules/orders/services/AddProductService.ts
+++ src/modules/orders/services/AddProductService.ts
@@ -1,3 +1,4 @@
+import { IProductsRepository } from '@modules/products/repositories/IProductsRepository';
 import { injectable, inject } from 'tsyringe';
 
 import ErrorsApp from '@shared/errors/ErrorsApp';
@@ -15,6 +16,9 @@ class AddProductService {
 
     @inject('ProductsOrderRepository')
     private productsOrderRepository: IProductsOrderRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
   ) {}
 
   public async execute(data: ICreateProductOrderDTO): Promise<IProductOrder> {
@@ -25,6 +29,23 @@ class AddProductService {
     if (!orderExists) {
       throw new ErrorsApp('Order not found', 404);
     }
+
+    const product = await this.productsRepository.findById(data.product_id);
+
+    if (!product) {
+      throw new ErrorsApp('Product not found', 404);
+    }
+
+    if (data.quantity > product.quantity) {
+      throw new ErrorsApp(
+        `We do not have ${data.quantity} itens ${product.name} in stock, only ${product.quantity}`,
+        409,
+      );
+    }
+
+    product.quantity -= Number(data.quantity);
+
+    await this.productsRepository.save(product);
 
     const index = orderExists.products.findIndex(
       item => item.product_id === data.product_id,
