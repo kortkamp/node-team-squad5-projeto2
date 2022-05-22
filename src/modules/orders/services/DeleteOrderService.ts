6bf6b41eb668e44b1ab80058f6b19e5b76a24fc7
kortkamp <marcelusmedius@gmail.com>
2022-05-22 11:27:24 -0300

    feat: return products to stock

--- src/modules/orders/services/DeleteOrderService.ts
+++ src/modules/orders/services/DeleteOrderService.ts
@@ -1,3 +1,4 @@
+import { IProductsRepository } from '@modules/products/repositories/IProductsRepository';
 import { inject, injectable } from 'tsyringe';
 
 import ErrorsApp from '@shared/errors/ErrorsApp';
@@ -9,13 +10,29 @@ class DeleteOrderService {
   constructor(
     @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
   ) {}
   public async execute(orderId: string) {
-    const order = await this.ordersRepository.findById(orderId);
+    const order = await this.ordersRepository.findById(orderId, ['products']);
 
     if (!order) {
       throw new ErrorsApp('Order not found', 404);
     }
+
+    // return all products to stock
+    const returnProducts = order.products.map(async productOrder => {
+      const product = await this.productsRepository.findById(
+        productOrder.product_id,
+      );
+
+      product.quantity += productOrder.quantity;
+
+      await this.productsRepository.save(product);
+    });
+
+    Promise.all(returnProducts);
 
     await this.ordersRepository.delete(order);
   }
--- src/modules/orders/services/DeleteProductOrderService.ts
+++ src/modules/orders/services/DeleteProductOrderService.ts
@@ -1,3 +1,4 @@
+import { IProductsRepository } from '@modules/products/repositories/IProductsRepository';
 import { inject, injectable } from 'tsyringe';
 
 import ErrorsApp from '@shared/errors/ErrorsApp';
@@ -14,15 +15,26 @@ class DeleteProductOrderService {
   constructor(
     @inject('ProductsOrderRepository')
     private productsOrderRepository: IProductsOrderRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
   ) {}
   public async execute({ id }: IRequest) {
-    const item = await this.productsOrderRepository.findById(id);
+    const productOrder = await this.productsOrderRepository.findById(id);
 
-    if (!item) {
+    if (!productOrder) {
       throw new ErrorsApp('Item not found', 404);
     }
 
-    await this.productsOrderRepository.delete(item);
+    const product = await this.productsRepository.findById(
+      productOrder.product_id,
+    );
+
+    product.quantity += productOrder.quantity;
+
+    await this.productsRepository.save(product);
+
+    await this.productsOrderRepository.delete(productOrder);
   }
 }
 
